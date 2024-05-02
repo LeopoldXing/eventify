8eceab171101dc6e45a4ada1d7055f0f1f24c33c
DiscoBroccoli <19515DearClem@gmail.com>
2024-05-02 16:31:55 -0500

    feat(footer): finish develop footer component

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -4,8 +4,8 @@ import Footer from "@/components/generic-layout/Footer";
 
 const RootLayout = ({children}: Readonly<{ children: React.ReactNode }>) => {
   return (
-      <div className="flex flex-col h-screen">
-        <main className="flex-1">
+      <div className="">
+        <main className="flex flex-col min-h-screen justify-between items-center">
           <Header/>
           {children}
           <Footer/>
--- components/generic-layout/Footer.tsx
+++ components/generic-layout/Footer.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
 
+import Image from 'next/image';
+import Link from "next/link";
+
 const Footer = () => {
   return (
-      <div>
-      </div>
+      <footer className="w-full border-t">
+        <div className="max-w-7xl p-5 md:px-10 xl:px-0 lg:mx-auto flex flex-col justify-center items-center space-x-0 space-y-4 md:flex-row md:justify-between">
+          <Link href="/">
+            <Image src="/assets/images/logo.svg" alt="Eventify Logo" width={128} height={38}/>
+          </Link>
+          <p>&copy; 2024 Eventify. All Rights Reserved</p>
+        </div>
+      </footer>
   );
 };
 
