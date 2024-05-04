19d0136a0166fabff844cbefa6a19dc2e23882b5
DiscoBroccoli <19515DearClem@gmail.com>
2024-05-04 14:13:54 -0500

    fix(user): remove requirement of the fields in UserSchema

--- lib/database/models/user.model.ts
+++ lib/database/models/user.model.ts
@@ -1,12 +1,12 @@
-import {Document, model, models, Schema} from "mongoose";
+import {model, models, Schema} from "mongoose";
 
 const UserSchema = new Schema({
   clerkId: {type: String, required: true, unique: true},
-  email: {type: String, required: true, unique: true},
-  username: {type: String, required: true, unique: true},
-  firsName: {type: String, required: true},
-  lastName: {type: String, required: true},
-  photo: {type: String, required: true}
+  email: {type: String, unique: true},
+  username: {type: String, unique: true},
+  firsName: {type: String},
+  lastName: {type: String},
+  photo: {type: String}
 })
 
 const User = models.User || model("User", UserSchema);
