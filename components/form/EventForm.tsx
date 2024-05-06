2cdcbace0808deb5a628392fcc46a2abfeec4be1
DiscoBroccoli <19515DearClem@gmail.com>
2024-05-05 23:05:15 -0500

    feat(create-event): working in progress on EventForm

--- components/form/EventForm.tsx
+++ components/form/EventForm.tsx
@@ -92,9 +92,8 @@ const EventForm = ({userId, type}: EventFormProps) => {
             <FormField control={form.control} name="location" render={({field}) => (
                 <FormItem className="w-full">
                   <FormControl>
-                    <div
-                        className="px-6 flex items-center justify-center overflow-hidden border-none rounded-lg bg-gray-100
-                                   focus:outline-none focus-visible:ring-transparent focus:ring-transparent !important">
+                    <div className="px-6 flex items-center justify-center overflow-hidden border-none rounded-lg bg-gray-100 focus:outline-none
+                                    focus-visible:ring-transparent focus:ring-transparent !important">
                       <Image src="/assets/icons/location-grey.svg" alt="calendar" width={24} height={24}/>
                       <Input placeholder="Event location or online" {...field} className="w-full form-input"/>
                     </div>
